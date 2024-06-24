dea31903d3272150da6a33ae89894499aff4ebfc
aswin531 <aswinkvaswinkv1@gmail.com>
2024-06-24 13:55:36 +0530

    Main Added MultiRepositoryProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'dart:async';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:rentit/core/constants/colors.dart';
 import 'package:rentit/core/constants/screen_util_setup.dart';
 import 'package:rentit/core/navigation/approutes.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:rentit/features/authentication/data/datasources/firebase_auth_datasource.dart';
+import 'package:rentit/features/authentication/data/repositories/auth_repo_imple.dart';
+import 'package:rentit/features/authentication/domain/repositories/auth_repository.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -12,7 +18,14 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(MultiRepositoryProvider(providers: [
+    RepositoryProvider<AuthRepository>(
+      create: (_) => AuthRepositoryImpl(
+        dataSource: FirebaseDataSource(
+            firebaseAuth: FirebaseAuth.instance, googleSignIn: GoogleSignIn()),
+      ),
+    ),
+  ], child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
